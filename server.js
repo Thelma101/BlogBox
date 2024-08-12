d988b5a3c89db12594cea18aeb0374e78b09739a
DCTP-BE <akpata.thelma@gmail.com>
2024-08-12 15:14:24 +0100

    mongodb connection to the database

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.port || 4000;
+
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/BlogBox', { useNewUrlParser: true, useUnifiedTopology: true })
+ .then(() => console.log('MongoDB Connected...Hurray!'))
+ .catch(err => console.log(err));
+
+ // Middleware to parse JSON request bodies
+//  app.user(express.user())
+
+app.listen(port, () => {
+    console.log(`Yaay! Server running on port ${port}`);
+  });
+

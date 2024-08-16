70c673e3bb21b089d92aa2cf53be5e46c4223cbc
DCTP-BE <akpata.thelma@gmail.com>
2024-08-16 21:10:03 +0100

    blogSchema update

--- src/models/blogBoxSchema.js
+++ src/models/blogBoxSchema.js
@@ -1,2 +1,14 @@
 const mongoose = require('mongoose');
 
+export default mongoose;
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  author: String,
+  date: { type: Date, default: Date.now },
+});
+
+const BlogBox = mongoose.model('BlogBox', postSchema);
+
+module.exports = BlogBox;

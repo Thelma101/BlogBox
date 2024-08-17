ef679b931bd3525535adfe6d1920fb431a091296
DCTP-BE <akpata.thelma@gmail.com>
2024-08-17 22:07:52 +0100

    npm install jsonwebtoken

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const app = express();
 const port = process.env.port || 4000;
 

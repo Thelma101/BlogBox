76978124570c39eda0c54a6cd38509c3e561108d
DCTP-BE <akpata.thelma@gmail.com>
2024-08-17 19:44:33 +0100

    blogSchema update

--- src/models/blogBoxSchema.js
+++ src/models/blogBoxSchema.js
@@ -1,2 +1,20 @@
 const mongoose = require('mongoose');
 
+const blogSchema = new blogSchema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+})

ed29d9917e31c4101b1d213eafe94926a64775fa
DCTP-BE <akpata.thelma@gmail.com>
2024-08-19 21:03:52 +0100

    comment schema

---
+++ src/models/commentSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new commentSchema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    blog: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Blog'
+    }
+});

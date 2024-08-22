ede7f47e9633f9ba5f4d78021ac8dc133395a3ca
DCTP-BE <akpata.thelma@gmail.com>
2024-08-22 16:05:03 +0100

    post blogblx update

--- src/controllers/blogBloxController.js
+++ src/controllers/blogBloxController.js
@@ -1,8 +1,10 @@
 
 app.post('/', async (req,res) {
     const { title, content, author, tag } = req.body;
+    if ( !title || !content || !author || !tag ) {
+        return res.status(404).json({ message: 'Please fill all the fields' })
+    }
 
-    
     try {
         // const newBlog = new Blog({
         //     title: req.body.title,
@@ -18,7 +20,11 @@ app.post('/', async (req,res) {
         // res.json({ message: 'New blog added successfully' });
 
         const newBlog = new Blog({
-
+            title,
+            content,
+            author,
+            tag
         })
+        
     }
 })

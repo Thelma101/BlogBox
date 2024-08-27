fc146272ecef8a1518785ecfd276e90b6965a0ea
DCTP-BE <akpata.thelma@gmail.com>
2024-08-27 22:08:40 +0100

    user route fix and dependencies update

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,11 +4,11 @@ const app = require('express').Router();
 
     app.post('/api/v1/user', user.createUser);
     app.get('/api/v1/user', user.getAllUsers);
-    // app.get('/api/v1/user', user.getUserById);
+    app.get('/api/v1/user/:id', user.getUser);
     app.put('/api/v1/user/:id', user.updateUser);
-    // app.delete('/api/v1/user', user.deleteUser);
-    // app.post('/api/v1/user/login', user.login);
-    // app.post('/api/v1/user/logout', user.logout);
+    app.delete('/api/v1/user', user.deleteUser);
+    app.post('/api/v1/user/login', user.login);
+    app.post('/api/v1/user/logout', user.logout);
 
 module.exports = app;
 

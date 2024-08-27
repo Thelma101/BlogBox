67cd3e24243f2ba28069aa979fce6d2eb7a31e8d
DCTP-BE <akpata.thelma@gmail.com>
2024-08-27 21:23:52 +0100

    user route fix and dependencies update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,23 +1,26 @@
-const userSchema = require('../models/userModel');
+const userSchema = require('../models/userSchema');
+const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
     const { username, email, password } = req.body;
     if (!username ||!email ||!password) {
-        return res.status(404).json({ message: 'Please fill all the fields' });
+        return res.status(400).json({ message: 'Please fill all the fields' });
     }
 
     try {
         const existingEmail = await userSchema.findOne({ email });
         const existingUser = await userSchema.findOne({ username });
-        if (existingUser) return res.status(409).json({ message: 'Username already exists' });
-        if (existingEmail) return res.status(409).json({ message: 'Email already exists' });
+        if (existingUser) return res.status(409).json({ message: 'Username already taken' });
+        if (existingEmail) return res.status(409).json({ message: 'Email address already in use' });
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new userSchema({ username, email, password: hashedPassword });
         await newUser.save();
 
         res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
+        console.error(error)
         res.status(500).json({ message: error.message, error });
     }
 }
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,11 +3,11 @@ const app = require('express').Router();
 
 
     app.post('/api/v1/user', user.createUser);
-    app.get('/api/v1/user', user.getAllUsers);
-    app.get('/api/v1/user', user.getUserById);
-    app.put('/api/v1/user', user.updateUser);
-    app.delete('/api/v1/user', user.deleteUser);
-    app.post('/api/v1/user/login', user.login);
-    app.post('/api/v1/user/logout', user.logout);
+    // app.get('/api/v1/user', user.getAllUsers);
+    // app.get('/api/v1/user', user.getUserById);
+    // app.put('/api/v1/user', user.updateUser);
+    // app.delete('/api/v1/user', user.deleteUser);
+    // app.post('/api/v1/user/login', user.login);
+    // app.post('/api/v1/user/logout', user.logout);
 
 module.exports = app;

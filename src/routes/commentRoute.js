d504788526726e4e03ec50b3ddb196baf82fbd5c
DCTP-BE <akpata.thelma@gmail.com>
2024-08-28 15:47:38 +0100

    comment controller file fix

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -1,6 +1,7 @@
 const commentRoute = require('../controllers/commentController');
 const app = require('express').Router();
 
-app.post = ('/api/v1/comment', commentRoute.createComment);
+app.post('/api/v1/comments', commentRoute.createComment);
+app.get('/api/v1/comments', commentRoute.getComment);
 
 module.exports = app;

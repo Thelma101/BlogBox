d48f127a26732d0ec1cac6ba3fb3db52e0c3af5a
DCTP-BE <akpata.thelma@gmail.com>
2024-08-28 15:38:57 +0100

    comment controller file fix

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,11 +1,15 @@
 
 exports.createComment = async (req, res) => {
-    const { content } = req.body;
-    if (!content) {
+    const { comment } = req.body;
+    if (!comment) {
         return res.status(404).send({ message: 'Field cannot be empty' })
     }
     try {
-        
+        const newComment = await commentSchema({
+            comment,
+            author: req.user._id,
+            blog_post: req.params.blogId
+        })
         const savedComment = await newComment.save();
         res.status(201).json(savedComment);
     } catch (error) {

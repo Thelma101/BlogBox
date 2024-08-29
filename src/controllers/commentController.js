4808e0ade6b030d4ebfd4ac241c521774a404f8f
DCTP-BE <akpata.thelma@gmail.com>
2024-08-29 18:43:00 +0100

    comment controller and route fixes

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,12 +1,42 @@
 const commentSchema = require('../models/commentSchema');
 const Blog = require('../models/blogSchema');
 
+// exports.createComment = async (req, res) => {
+//     const { comment } = req.body;
+//     const blogId = req.params.blogId;
+//     console.log('Blog Id: ', blogId)
+//     if (!comment) {
+//         return res.status(400).send({ message: 'Field cannot be empty' })
+//     }
+
+//     try {
+//         const blogExist = await Blog.findById(blogId);
+//         if (!blogExist) {
+//             return res.status(404).json({ message: 'Blog not found' });
+//         }
+
+//         const newComment = new Comment({
+//             comment,
+//             author: req.user._id,
+//             blog: blogId
+//         });
+
+//         const savedComment = await newComment.save();
+//         console.log('Comment saved:', savedComment);
+//         res.status(201).json(savedComment);
+//     } catch (error) {
+//         console.error('Error saving comment:', error.message, error.stack);
+//         res.status(500).json({ message: 'Internal Server Error', error: error.message });
+
+//     }
+// };
+
 exports.createComment = async (req, res) => {
     const { comment } = req.body;
     const blogId = req.params.blogId;
-    console.log('Blog Id: ', blogId)
+
     if (!comment) {
-        return res.status(400).send({ message: 'Field cannot be empty' })
+        return res.status(400).send({ message: 'Field cannot be empty' });
     }
 
     try {
@@ -22,14 +52,14 @@ exports.createComment = async (req, res) => {
         });
 
         const savedComment = await newComment.save();
-        console.log('Comment saved:', savedComment);
+        blogExist.comments.push(savedComment._id);
+        await blogExist.save();
+
         res.status(201).json(savedComment);
     } catch (error) {
-        console.error('Error saving comment:', error.message, error.stack);
         res.status(500).json({ message: 'Internal Server Error', error: error.message });
-
     }
-}
+};
 
 exports.getAllComments = async (req, res) => {
     try {
@@ -39,4 +69,4 @@ exports.getAllComments = async (req, res) => {
         console.error(error);
         res.status(500).json({ message: error.message, error: error.stack });
     }
-}
+};

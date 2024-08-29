07dbc64535aa30c946e4b49379d2a6686be0abe9
DCTP-BE <akpata.thelma@gmail.com>
2024-08-29 11:51:06 +0100

    comment controller and route fixes

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,33 +3,43 @@ const blogSchema = require('../models/blogSchema');
 
 exports.createComment = async (req, res) => {
     const { comment } = req.body;
-    if (!comment) {
-        return res.status(404).send({ message: 'Field cannot be empty' })
-    }
     const blogId = req.params.blogId;
+    if (!comment) {
+        return res.status(400).send({ message: 'Field cannot be empty' })
+    }
+
     const blogExist = await blogSchema.findById(blogId);
     if (!blogExist) {
         return res.status(404).json({ message: 'Blog not found' });
     }
     try {
-        const newComment = await commentSchema({
+        const blogExist = await Blog.findById(blogId);
+        if (!blogExist) {
+            return res.status(404).json({ message: 'Blog not found' });
+        }
+
+        const newComment = new Comment({
             comment,
             author: req.user._id,
             blog: blogId
-        })
+        });
+
         const savedComment = await newComment.save();
+        console.log('Comment saved:', savedComment); 
         res.status(201).json(savedComment);
     } catch (error) {
-        res.status(500).json({ message: error.message, error });
+        console.error('Error saving comment:', error.message, error.stack);  
+        res.status(500).json({ message: 'Internal Server Error', error: error.message });
+
     }
 }
 
-exports.getAllComments = async (req, res) => {
-    try {
-        const comments =  await commentSchema.find();
-        res.json(comments);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: error.message, error: error.stack });
+    exports.getAllComments = async (req, res) => {
+        try {
+            const comments = await commentSchema.find();
+            res.json(comments);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: error.message, error: error.stack });
+        }
     }
-}
